2d1f35a53a1522d338a1ae89ed586a956bb907dd
NHD <tt2.nhd@outlook.com>
2021-10-17 00:08:19 +0700

    Update Personnal Final project

--- Final/tailwind.config.js
+++ Final/tailwind.config.js
@@ -10,39 +10,36 @@ module.exports = {
           'center': true
       },
       extend: {
-          fontFamily: {
-              'raleway': "'Raleway', sans-serif",
-          },
+        fontFamily: {
+            'raleway': "'Raleway', sans-serif",
+        },
           fontSize: {
-              //desktop
-              'dh1': ["48px", "56.35px"],
-              'dh2': ["40px", "46.96px"],
-              'dh3': ["32px", "37.57px"],
-              'dh4': ["24px", "28.18px"],
-              //mobile
-              'mh1': ["40px", "46.96px"],
-              'mh2': ["32px", "32.57px"],
-              'mh3': ["24px", "28.18px"],
-              'mh4': ["20px", "23.48px"],
-              'dh1': ["48px", "56.35px"],
-              //common
-              'subtitle': ["20px", "23.48px"],
-              'label': ["16px", "18.78px"],
-              'body': ["16px", "18.78px"],
-              'caption': ["13px", "15.26px"],
-          },
-
-          
-          
-      },
+            //desktop
+            'dh1': ["48px", "56.35px"],
+            'dh2': ["40px", "46.96px"],
+            'dh3': ["32px", "37.57px"],
+            'dh4': ["24px", "28.18px"],
+            //mobile
+            'mh1': ["40px", "46.96px"],
+            'mh2': ["32px", "32.57px"],
+            'mh3': ["24px", "28.18px"],
+            'mh4': ["20px", "23.48px"],
+            'dh1': ["48px", "56.35px"],
+            //common
+            'subtitle': ["20px", "23.48px"],
+            'label': ["16px", "18.78px"],
+            'body': ["16px", "18.78px"],
+            'caption': ["13px", "15.26px"],
+            },
+        
+         },
 
 
-      variants: {
-
-          extend: {}
-      },
+        variants: {
+            extend: {}
+        },
   },
-  plugins: [
+    plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
     // ...
